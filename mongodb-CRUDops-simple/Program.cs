81b346b765306286b3f0bea5ad74a27ece6dcef6
Theper10 <thfelio@gmail.com>
2025-12-30 12:19:16 +0100

    Initial commit

---
+++ mongodb-CRUDops-simple/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace mongodb_CRUDops_simple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
